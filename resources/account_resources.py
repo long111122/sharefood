e3c762f3612e0c2f96e59fd6f8269a57664ef323
Edgy <longttse04476@fpt.edu.vn>
2017-06-10 14:56:34 +0700

    no message

--- resources/account_resources.py
+++ resources/account_resources.py
@@ -2,14 +2,5 @@ from flask_restful import Resource, reqparse
 from models.account import Account
 
 class ListAccount(Resource):
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument(username="username", type=str, location=json)
-        parser.add_argument(password="password", type=str, location=json)
-        body = parser.parse_args()
-        username = body["username"]
-        password = body["password"]
-        account = Account(username=username, password=password)
-        account.save()
-
-        return {"username":username, "password":password}
+    def get(self):
+        return {"hello world" : "fking shit"}
